5b2f618c4e15acd5d810a4ee85c703bcdb9a0b3a
Christopher Jackson <jacksoch1994@gmail.com>
2023-04-11 14:43:57 -0400

    Added routing rules based on user type to portal pages.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,19 +2,12 @@
   <div id="app">
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      <router-link
+        v-bind:to="{ name: 'logout' }"
+        v-if="$store.state.token != ''"
+        >Logout</router-link
+      >
     </div>
     <router-view />
   </div>
 </template>
-
-<script>
-import { defineComponent } from '@vue/composition-api'
-
-export default defineComponent({
-  setup() {
-    
-  },
-  
-})
-</script>
