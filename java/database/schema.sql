52d71c9448072c169306b94825dc50c6a97c25d5
stephen hendricks <schendricks@gmail.com>
2023-04-11 10:31:33 -0400

    Create properties table in database

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -8,6 +8,31 @@ CREATE TABLE users (
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
-);
+    );
+
+	CREATE TABLE properties (
+	    property_id SERIAL,
+	    name varchar(50) NOT NULL UNIQUE,
+	    street_address varchar(50) NOT NULL,
+	    unit numeric (10) NOT NULL,
+	    state varchar(2) varchar(2),
+	    zip numeric(10),
+	    bedrooms numeric(10),
+	    bathrooms numeric(10),
+	    square_footage numeric(4000),
+	    owner_id int NOT NULL,
+	    description varchar(900),
+	    image_url NVARCHAR(2083),
+    );
+
+    CREATE TABLE leases (
+        lease_id SERIAL,
+        tenant_id,
+        property_id,
+        rent_amount,
+        start_date,
+        end_date,
+
+    );
 
 COMMIT TRANSACTION;
