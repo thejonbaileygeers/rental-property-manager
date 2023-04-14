a11f5de4426b02f96866a4b93ed2d54bd14bb664
Christopher Jackson <jacksoch1994@gmail.com>
2023-04-14 18:44:26 -0400

    Add minor formatting to Tenant Portal and prototype Property Info 'Widget' to portal.

--- vue/src/views/TenantPortal.vue
+++ vue/src/views/TenantPortal.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
-    <h1>Welcome to the Tenant Portal!!</h1>
-    <br /><placeholder-greeting />
-    <maintenance-request-list :property="property" />
+    <div id="portal">
+      <h1>Welcome to the Tenant Portal!!</h1>
+      <br /><placeholder-greeting />
+      <div id="content">
+        <tenant-property-display :property="property" />
+        <maintenance-request-list :property="property" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import MaintenanceRequestList from "../components/MaintenanceRequestList.vue";
 import PlaceholderGreeting from "../components/PlaceholderGreeting.vue";
+import TenantPropertyDisplay from "../components/Tenant-Property-Display.vue";
 
 export default {
   data() {
@@ -16,7 +22,11 @@ export default {
       property: {},
     };
   },
-  components: { PlaceholderGreeting, MaintenanceRequestList },
+  components: {
+    PlaceholderGreeting,
+    MaintenanceRequestList,
+    TenantPropertyDisplay,
+  },
   created() {
     const lease = this.$store.state.leases.find((lea) => {
       return lea.tenantId == this.$store.state.user.id;
@@ -31,5 +41,15 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+#content {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-evenly;
+  gap: 2rem;
+}
+
+#portal {
+  padding: 4rem;
+}
 </style>
