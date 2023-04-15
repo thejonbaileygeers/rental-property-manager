9e0edaae9f3e57d19e2a5b542fd1a36fc2a22730
Christopher Jackson <jacksoch1994@gmail.com>
2023-04-15 15:11:27 -0400

    Add missing methods in Front-End Axios Services to reflect endpoints in Back-End controllers.

--- vue/src/services/LeaseService.js
+++ vue/src/services/LeaseService.js
@@ -3,5 +3,13 @@ import axios from "axios";
 export default {
     getAll() {
         return axios.get('/leases');
+    },
+
+    createLease(lease) {
+        return axios.post('/leases', lease)
+    },
+
+    updateLease(leaseId, lease) {
+        return axios.put(`/leases/${leaseId}`, lease);
     }
 }
--- vue/src/services/MaintRequestService.js
+++ vue/src/services/MaintRequestService.js
@@ -8,5 +8,13 @@ export default {
 
     create(request) {
         return axios.post("/maintenance", request);
+    },
+
+    assignMaintenance(requestID, maintenanceId) {
+        return axios.patch(`/maintenance/${requestID}`, { maintenanceId });
+    },
+
+    completeRequest(requestID) {
+        return axios.patch(`/maintenance/${requestID}/complete`);
     }
 }
