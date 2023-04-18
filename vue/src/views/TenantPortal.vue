a516d0fbb0b1b1182eb4ed07ffde8033774df125
Jonathan Arias <jmarias123@gmail.com>
2023-04-18 08:39:34 -0400

    TenantDashMaintRequestDetailsStyling

--- vue/src/views/TenantPortal.vue
+++ vue/src/views/TenantPortal.vue
@@ -1,12 +1,18 @@
 <template>
-  <div>
-    <div id="portal">
-      <h1>Welcome to the Tenant Portal!!</h1>
-      <br /><placeholder-greeting />
-      <div id="content">
-        <tenant-property-display :property="property" />
-        <maintenance-request-list :property="property" />
+  <div class="list">
+    <div class="header">
+      <img src="../assets/skyline-crop-3.jpg" alt="Skyline" />
+      <div class="title">
+        <h1>Welcome to the <span>Tenant Portal</span></h1>
+        <div class="placeholder">
+        <br /><placeholder-greeting />
+        </div>
       </div>
+    </div>
+    <div id="black-bar" />
+    <div class="content">
+      <tenant-property-display :property="property" />
+      <maintenance-request-list :property="property" />
     </div>
   </div>
 </template>
@@ -40,14 +46,65 @@ export default {
 </script>
 
 <style scoped>
-#content {
+.content {
   display: flex;
   align-items: flex-start;
   justify-content: space-evenly;
   gap: 2rem;
+  padding: 2rem 7rem;
 }
 
-#portal {
-  padding: 4rem;
+.header h1,
+.header p {
+  color: white;
+  text-align: center;
+  align-content: center;
+  font-weight: 500;
+  text-shadow: 1px 1px 10px black;
+}
+
+.placeholder{
+  color: white;
+  text-align: center;
+  align-content: center;
+  font-weight: 500;
+  text-shadow: 1px 1px 10px black;
+
+}
+
+.header h1 {
+  font-size: 2.8rem;
+}
+
+.header p {
+  font-size: 1.5rem;
+}
+
+.title {
+  position: absolute;
+  top: 9rem;
+  left: 50%;
+  transform: translate(-50%, 0%);
+  width: 100%;
+}
+
+.header {
+  height: 20rem;
+  overflow: hidden;
+}
+
+img {
+  max-width: 100%;
+  display: block;
+  height: auto;
+}
+
+.title span {
+  font-style: italic;
+}
+
+#black-bar {
+  height: 0.2rem;
+  background-color: black;
 }
 </style>
